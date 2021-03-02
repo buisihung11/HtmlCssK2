c8536e2d01cbbc01d8999f1fcb8b3f890acdaa45
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2021-03-02 20:25:30 +0700

    Add excercise

---
+++ Readme.md
@@ -0,0 +1,87 @@
+number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+bigint is for integer numbers of arbitrary length.
+string for strings. A string may have zero or more characters, there’s no separate single-character type.
+boolean for true/false.
+null for unknown values – a standalone type that has a single value null.
+undefined for unassigned values – a standalone type that has a single value undefined.
+object for more complex data structures.
+symbol for unique identifiers.In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+## Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+Count Change	Cards
++1	2, 3, 4, 5, 6
+0	7, 8, 9
+-1	10, 'J', 'Q', 'K', 'A'
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Output
+-3 Hold
+5 Bet
+
+Hint
+Do NOT reset count to 0 when value is 7, 8, or 9.
+Do NOT return an array.
+Do NOT include quotes (single or double) in the output.
+
+
+## In the game of golf each hole has a par meaning the average number of strokes a golfer is expected to make in order to sink the ball in a hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
+
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
+
+
+## Bai tap
+
+1. Check palindrome cua mot chu, nguoc voi nhau aoa khi nguoc lai van la aoa (car -> rac -> FALSE)
+2. Thay the cac khoang trang bang dau - (day la link url-> day-la-link-url)
+3. In hoa nhung ky tu dau cua chu (hung bui -> Hung Bui)
+4. Cat mot chu thanh cac cum ki tu theo chieu dai cho truoc
+   hungbui , 2 -> ["hu","ng","bu","i"]
+   ball, 2 -> ["ba","ll"]
+   ball, 5 -> ["ball"]
+5. Tim so lan xuat hien cua mot tu trong mot chuoi cho truong 
+   hung hung hung1 , hung --> 2
+
+# Excersize 02/03/2021
+
+## 1. Ex1: 
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+## 2.Ex2: 
+
+Find intersection of two array
+Ex: 
+intersection([2, 1], [2, 3]);
+// => [2]
+
+## 3.Ex3: 
+Union of two array   
+union([2], [1, 2]);
+// => [2, 1]
+
+## 4.Ex4:
+Xor all equal value ()
+xor([2, 1], [2, 3]);
+// => [1, 3]
+
+## 5.Ex5: 
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+```
+fearNotLetter("abce") should return "d".
+```
---
+++ index.js
@@ -0,0 +1,235 @@
+// for (let index = 0; index < 3; index++) {
+//   let star = Array(index + 1).fill("*");
+//   console.log(star.join(""));
+// }
+
+// for (let index = 0; index < 3; index++) {
+//   let space = Array(3 - (index + 1)).fill(" ");
+//   let star = Array(index + 1).fill("*");
+//   console.log(space.join("") + star.join(""));
+// }
+
+// STRING METHOD, ATTRIBUTE
+
+let str = "      Hello World !!!🎈🎈OOO World     ";
+console.log(str);
+
+let strLength = str.length;
+
+console.log("strLength", strLength);
+
+// TEMPLATE STRING
+// length
+console.log(`Chieu dau chuoi la ${str.length}`);
+
+// Ham split
+console.log(str.split("🎈"));
+
+// nth by bracket
+console.log(str[str.length - 1]);
+
+// FInd word
+console.log(str.indexOf("World"));
+
+// Substring Cat chuoi
+const subStr = str.substring(0, 5);
+console.log(subStr);
+console.log(str);
+
+console.log(str.slice(0, 5));
+console.log(str);
+
+let a = "A";
+let b = "B";
+
+let strCon = str.concat(". Tra thai", a, b);
+console.log("str", str);
+console.log("strCon", strCon);
+
+// CHAIN FUNCTIONS
+console.log("str.length", str.length);
+console.log("str.trim().length", str.trim().length);
+let strTRim = str.trim();
+console.log("strTRim.length", strTRim.length);
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+let test = "hung";
+console.log(test.length);
+
+let testStr = "The quick brown fox jumped over the lazy dogs";
+// console.log(testStr.replace(/\s+/g,""))
+// 1. IN RA SO KI TU CUA MOT CHUOI CHO TRUOC (HELLO -> 5)
+console.log(testStr.length);
+// 2. IN RA LUONG SO CHU CUA MOT CHUOI CHO TRUOC (HELLO WORLD -> 2)
+
+let arr = "hung   bui".split(" ");
+
+console.log(arr);
+
+let countWord = 0;
+
+for (let index = 0; index < arr.length; index++) {
+  let word = arr[index];
+  console.log(word);
+  if (word.length != 0) {
+    countWord++;
+  }
+}
+console.log(countWord);
+
+// 3.
+let testStrArr = testStr.split(" ");
+console.log(testStrArr);
+let max = testStrArr[0];
+
+for (let index = 0; index < testStrArr.length; index++) {
+  const word = testStrArr[index];
+  console.log(word);
+  if (max.length < word.length) {
+    max = word;
+  }
+}
+
+console.log(max);
+console.log(max.length);
+
+// 4.
+let test4 = "a12bc"; // cba
+
+let test4Arr = test4.split("");
+console.log(test4Arr);
+
+for (let index = 0; index < test4Arr.length / 2; index++) {
+  let tmp = test4Arr[index];
+  test4Arr[index] = test4Arr[test4Arr.length - 1 - index];
+  test4Arr[test4Arr.length - 1 - index] = tmp;
+}
+
+console.log(test4Arr);
+let reverseStr4 = test4Arr.join("");
+console.log(reverseStr4);
+
+// SOL 2:
+let reverseStrr = "";
+
+for (let index = testStr.length - 1; index >= 0; index--) {
+  reverseStrr += testStr[index];
+}
+
+console.log(reverseStrr);
+
+// REGULAR EXPRESSION
+let splittedWords = "hung  bui".split(/\s+/);
+// let splittedWords = "hung  bui".split(" ");
+console.log(splittedWords.length);
+
+// 3. TIM RA CHU CO CHIEU DAI LON NHAT TRONG CHUOI (HELLO WORLD -> HELLO, HUNG BUI -> HUNG)
+
+let words = testStr.split(" ");
+
+let longestWord = null;
+for (let index = 0; index < words.length; index++) {
+  const word = words[index];
+  if (longestWord == null) longestWord = word;
+  if (word.length > longestWord.length) {
+    longestWord = word;
+  }
+}
+
+console.log(
+  `Chu co chieu dai lon nhat la ${longestWord} co chieu dai la ${longestWord.length}`
+);
+
+// 4. DAO CHU, abc -> cba
+let reverseStr = "";
+
+for (let index = testStr.length - 1; index >= 0; index--) {
+  reverseStr += testStr[index];
+}
+
+console.log(reverseStr);
+
+// ARRAY
+// METHOD, POP,PUSH,SHIFT,UNSHIFT
+// INDEX GET
+
+// EX: sumAll([1, 4]);
+
+// OBJECT
+// ACCESS WITH DOT,BRACKET, VARIABLE
+
+// FOR OBJECT
+
+// LOOP ARRAY
+
+// SWITCH, TERMIATE OPERATION
+// EX: PRINT EMOJI BASE ON TEXT
+
+// FUNCTION
+let number = 6;
+
+let number2;
+
+if (number > 5) {
+  number2 = 10;
+} else {
+  number2 = 1;
+}
+
+number2 = number > 5 ? 10 : 1;
+
+console.log(number2);
+
+switch (number) {
+  case 5:
+    console.log('Bang 5');
+    break;
+  default:
+    console.log('404');
+    break;
+}
+
+
+function reverseStrFunc(testStr) {
+  let reverseStrr = "";
+
+  for (let index = testStr.length - 1; index >= 0; index--) {
+    reverseStrr += testStr[index];
+  }
+  console.log(reverseStrr);
+  return reverseStrr;
+}
+
+function isStrongPass(password) {
+
+  return password.length > 8;
+}
+
+function checkNumber(number) {
+  return number > 10 ? 'Greater than 10' : 'Not Greater than 10';
+}
+
+console.log(checkNumber(5))
+
+console.log(isStrongPass('hung12345'))
+
+
+let afterReverseStr = reverseStrFunc('hung');
+console.log(afterReverseStr);
+
+function addV1(a, b) {
+  return a + b;
+}
+
+let addV2 = function (a, b) {
+  return a + b;
+}
+
+let addV3 = (a, b) => a + b; // cach moi
+
+let test123 = addV2(1, 2);
+console.log(test123);
+
+
